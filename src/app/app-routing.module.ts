7ff895fa2b2ddd83b07f844332cb7297e1b7ce98
rakeshdasari11 <52665040+rakeshdasari11@users.noreply.github.com>
2021-05-17 21:23:37 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ManageUserComponent } from './user/manage-user/manage-user.component';
+import { UserProfileComponent } from './user/user-profile/user-profile.component';
+
+const routes: Routes = [
+ 
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  { path: 'user-profile/:id', component: UserProfileComponent },
+  { path: 'user-profile/', component: UserProfileComponent },
+  { path: 'manage-user', component: ManageUserComponent },
+ 
+  { path: '**', redirectTo: '/home' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/custom-validators.service.ts
@@ -0,0 +1,35 @@
+import { FormControl, Validators } from '@angular/forms';
+export class CustomValidators extends Validators {
+
+  static validateImage(control: FormControl) {
+    if (control.value && control.value.length > 0) {
+      console.log(control.value);
+      console.log(control.value.file);
+      if (control.value.endsWith('.png') || control.value.endsWith('.jpg') || control.value.endsWith('.jpeg') ||control.value.endsWith('.PNG')) {
+        console.log("if")
+        return null;
+      } else {
+        console.log("else")
+        return { invalidImage: true };
+      }
+    } else {
+      return null;
+    }
+  }
+
+  static validateName(control: FormControl) {
+    if (control.value && control.value.length > 0) {
+      const regex = /^([a-zA-Z ]){2,20}$/;
+      if (regex.test(control.value)) {
+     
+        return null;
+      }
+      else {
+       
+        return { invalid: true };
+      }
+    } else {
+      return null;
+    }
+  }
+} 
